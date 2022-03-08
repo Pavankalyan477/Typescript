001c9267ef6d902fb99b623599b19205c216dfd8
Pavankalyan477 <sarakantipavankalyan070@gmail.com>
2022-03-08 18:21:30 +0530

    working

--- GettingStarted/app.js
+++ GettingStarted/app.js
@@ -1,10 +1,12 @@
 var person = {
     name: "pavan",
     age: 30,
-    hobbies: ["dance", "Sports"]
+    hobbies: ["dance", "Sports"],
+    role: [1, "admin"]
 };
 var arr = [1, 2];
-console.log('object' === 'object');
+person.role.push("new");
+console.log(person.role);
 for (var key in person) {
     if (typeof key === typeof arr) {
         console.log(person[key]);
--- GettingStarted/app.ts
+++ GettingStarted/app.ts
@@ -2,12 +2,14 @@ const person={
     name:"pavan",
     age:30,
     hobbies:["dance","Sports"],
+    role:[1,"admin"]
 
 
 
 }
 let arr=[1,2]
-console.log('object'==='object')
+person.role.push("new")
+console.log(person.role)
 for(let key in person){
     if(typeof key===typeof arr){
         console.log(person[key])
